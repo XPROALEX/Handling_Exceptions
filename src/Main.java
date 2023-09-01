14be218fc27e843fe43d49fdfeabdc9da1b56040
Alex <alex97av@gmail.com>
2023-09-01 12:15:36 +0200

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
 public class Main {
+    public static void main(String[] args) {
+        try {
+            //double inputScore = 2.15;
+            //double inputScore = 50;
+            //double inputScore = 95.02;
+            double inputScore = 100.01;
+            checkScore(inputScore);
+        } catch (ArithmeticException arithmeticException) {
+            System.out.println("Error: " + arithmeticException.getMessage());
+        }
+    }
+
     public static void checkScore(double score) {
         if (0 < score && score <= 50.00) {
             System.out.println("Average score!");
@@ -6,18 +18,6 @@ public class Main {
             System.out.println("Very good score!");
         } else {
             throw new ArithmeticException("Score is out of scale");
-        }
-    }
-
-    public static void main(String[] args) {
-        try {
-            // double inputScore = 2.15;
-            //double inputScore = 50;
-            //double inputScore = 95.02;
-             double inputScore = 100.01;
-            checkScore(inputScore);
-        } catch (ArithmeticException arithmeticException) {
-            System.out.println("Error: "+arithmeticException.getMessage());
         }
     }
 }
